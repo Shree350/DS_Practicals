34fef58e3d2d6c8654ff8b72ad2758f2788b89b5
Shreeraj Karval <karvalshreeraj45@gmail.com>
2026-04-22 10:45:21 +0530

    Refactor Experiment9 to BinaryTree with createTree method

--- Experiment9.java
+++ Experiment9.java
@@ -1,86 +1,118 @@
-
-
 import java.util.Scanner;
+
 class node {
     int data;
-    node left,right;
+    node left, right;
 
-    node(int data){
+    node(int data) {
         this.data = data;
         left = right = null;
     }
 }
 
-public class Experiment9 {
+public class BinaryTree {
     node root;
+    Scanner sc = new Scanner(System.in);
 
-    void inOrderDisplay(node root){
-        if(root != null){
+    // 🔥 Create Tree
+    node createTree() {
+        System.out.print("Enter data (-1 for NULL): ");
+        int data = sc.nextInt();
+
+        if (data == -1)
+            return null;
+
+        node newNode = new node(data);
+
+        System.out.println("Enter left child of " + data);
+        newNode.left = createTree();
+
+        System.out.println("Enter right child of " + data);
+        newNode.right = createTree();
+
+        return newNode;
+    }
+
+    // Traversals
+    void inOrderDisplay(node root) {
+        if (root != null) {
             inOrderDisplay(root.left);
             System.out.print(root.data + " ");
             inOrderDisplay(root.right);
         }
     }
-    void preOrderDisplay(node root){
-        if(root != null){
+
+    void preOrderDisplay(node root) {
+        if (root != null) {
             System.out.print(root.data + " ");
             preOrderDisplay(root.left);
             preOrderDisplay(root.right);
         }
     }
 
-    void postOrderDisplay(node root){
-        if(root != null){
+    void postOrderDisplay(node root) {
+        if (root != null) {
             postOrderDisplay(root.left);
             postOrderDisplay(root.right);
             System.out.print(root.data + " ");
         }
     }
-    public static void main(String[] args) {
-        
-        Scanner sc = new Scanner(System.in);
-        Experiment9 tree = new Experiment9();
 
-        tree.root = new node(1);
-        tree.root.left = new node(5);
-        tree.root.right = new node(10);
-        tree.root.left.left = new node(8);
-        tree.root.left.right = new node(9);
-        tree.root.right.left = new node(11);
-        tree.root.right.right = new node(13);
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+        Scanner sc = new Scanner(System.in);
 
         int choice;
 
         do {
             System.out.println("\n------ MENU ------");
-            System.out.println("1. Inorder");
-            System.out.println("2. Preorder");
-            System.out.println("3. Postorder");
-            System.out.println("4. Exit");
+            System.out.println("1. Create Tree");
+            System.out.println("2. Inorder");
+            System.out.println("3. Preorder");
+            System.out.println("4. Postorder");
+            System.out.println("5. Exit");
             System.out.print("Enter choice: ");
 
             choice = sc.nextInt();
 
             switch (choice) {
+
                 case 1:
-                    System.out.print("--------Inorder Tree-------- \n\n");
-                    tree.inOrderDisplay(tree.root);
-                    System.out.println();
+                    System.out.println("--------Create Tree--------");
+                    tree.root = tree.createTree();
                     break;
 
                 case 2:
-                    System.out.print("-------Preorder Tree--------- \n\n");
-                    tree.preOrderDisplay(tree.root);
-                    System.out.println();
+                    if (tree.root == null)
+                        System.out.println("Tree is empty");
+                    else {
+                        System.out.print("--------Inorder Tree--------\n\n");
+                        tree.inOrderDisplay(tree.root);
+                        System.out.println();
+                    }
                     break;
 
                 case 3:
-                    System.out.print("------Postorder Tree--------- \n\n");
-                    tree.postOrderDisplay(tree.root);
-                    System.out.println();
+                    if (tree.root == null)
+                        System.out.println("Tree is empty");
+                    else {
+                        System.out.print("--------Preorder Tree--------\n\n");
+                        tree.preOrderDisplay(tree.root);
+                        System.out.println();
+                    }
                     break;
 
                 case 4:
+                    if (tree.root == null)
+                        System.out.println("Tree is empty");
+                    else {
+                        System.out.print("--------Postorder Tree--------\n\n");
+                        tree.postOrderDisplay(tree.root);
+                        System.out.println();
+                    }
+                    break;
+
+                case 5:
                     System.out.println("Exiting...");
                     break;
 
@@ -88,10 +120,8 @@ public class Experiment9 {
                     System.out.println("Invalid choice!");
             }
 
-        } while (choice != 4);
+        } while (choice != 5);
 
         sc.close();
-        
     }
-    
 }
