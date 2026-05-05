b3873884d1ff35217359f963f6c57698d88e9e41
Shreeraj Karval <karvalshreeraj45@gmail.com>
2026-05-05 15:25:52 +0530

    Refactor Experiment1 to support dynamic array size

--- Experiment1.java
+++ Experiment1.java
@@ -1,70 +1,106 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Experiment1 {
-    public static void main(String[] args) {
+public class Exp1 {
+
+    public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
 
-        int i;
-        int a[] = new int[5];
-        int b[] = new int[5];
-        int c[] = new int[5];
-        int ch, sum = 0;
-        char x;
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int a[] = new int[n];
+        int b[] = new int[n];
+        int c[] = new int[n];
+
+        int choice;
 
         do {
-            System.out.println("\n1. Create");
-            System.out.println("2. Display");
-            System.out.println("3. Sum");
-            System.out.println("4. Substraction");
-            System.out.print("Enter your choice: ");
-            ch = sc.nextInt();
+            System.out.println("\n\n----MENU----");
+            System.out.println("1. Create A");
+            System.out.println("2. Create B");
+            System.out.println("3. Display A");
+            System.out.println("4. Display B");
+            System.out.println("5. Addition");
+            System.out.println("6. Subtraction");
+            System.out.println("7. Multiplication");
+            System.out.println("8. Division");
+            System.out.println("9. Exit");
 
-            switch(ch) {
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+
                 case 1:
-                    System.out.print("Enter 5 elements for array a: ");
-                    for(i = 0; i < 5; i++)
+                    System.out.print("Enter elements of A: ");
+                    for (int i = 0; i < n; i++) {
                         a[i] = sc.nextInt();
+                    }
                     break;
 
                 case 2:
-                    System.out.print("Elements in array a are: ");
-                    for(i = 0; i < 5; i++)
-                        System.out.print(a[i] + " ");
-                    System.out.println();
+                    System.out.print("Enter elements of B: ");
+                    for (int i = 0; i < n; i++) {
+                        b[i] = sc.nextInt();
+                    }
                     break;
 
                 case 3:
-                    sum = 0;
-                    for(i = 0; i < 5; i++)
-                        sum += a[i];
-                    System.out.println("Sum = " + sum);
+                    System.out.print("Array A: ");
+                    for (int i = 0; i < n; i++) {
+                        System.out.print(a[i] + " ");
+                    }
                     break;
 
                 case 4:
-                    System.out.print("Enter 5 elements for array b: ");
-                    for(i = 0; i < 5; i++)
-                        b[i] = sc.nextInt();
+                    System.out.print("Array B: ");
+                    for (int i = 0; i < n; i++) {
+                        System.out.print(b[i] + " ");
+                    }
+                    break;
 
-                    System.out.print("Subtraction (a - b) = ");
-                    for(i = 0; i < 5; i++) {
+                case 5:
+                    System.out.print("Addition: ");
+                    for (int i = 0; i < n; i++) {
+                        c[i] = a[i] + b[i];
+                        System.out.print(c[i] + " ");
+                    }
+                    break;
+
+                case 6:
+                    System.out.print("Subtraction: ");
+                    for (int i = 0; i < n; i++) {
                         c[i] = a[i] - b[i];
                         System.out.print(c[i] + " ");
                     }
-                    System.out.println();
+                    break;
+
+                case 7:
+                    System.out.print("Multiplication: ");
+                    for (int i = 0; i < n; i++) {
+                        c[i] = a[i] * b[i];
+                        System.out.print(c[i] + " ");
+                    }
+                    break;
+
+                case 8:
+                    System.out.print("Division: ");
+                    for (int i = 0; i < n; i++) {
+                        c[i] = a[i] / b[i];
+                        System.out.print(c[i] + " ");
+                    }
+                    break;
+
+                case 9:
+                    System.out.println("Exit...");
                     break;
 
                 default:
-                    System.out.println("Invalid choice");
+                    System.out.println("Invalid choice!");
             }
 
-            System.out.print("Continue? (y/n): ");
-            x = sc.next().charAt(0);
+        } while (choice != 9);
 
-        } while(x == 'y' || x == 'Y');
-
-        sc.close();
     }
 }
-
-
